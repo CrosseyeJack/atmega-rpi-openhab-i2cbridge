bf5db53e6d3273059e01f83a5c7d89c58b5312bf
Crosseye Jack <jack@crosseyejack.com>
2014-09-16 17:46:07 +0100

    Linked wiringPi and wiringPiI2C
    And cooking a nice chilli :-)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 /* 
  * File:   main.cpp
- * Author: srv
+ * Author: Daniel Williams
  *
  * Created on 16 September 2014, 16:14
  */
 
 #include <cstdlib>
+#include <wiringPiI2C.h>
 
 using namespace std;
 
