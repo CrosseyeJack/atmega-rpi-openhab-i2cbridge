72e5b5ce33092543398fea5b927c0bd34e886dbb
Daniel Williams <dan@crosseyejack.com>
2014-09-16 16:20:06 +0100

    Blank netbeans project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: srv
+ *
+ * Created on 16 September 2014, 16:14
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
