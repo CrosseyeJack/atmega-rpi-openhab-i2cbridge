83d2b55a7e7a672b8b8ccbf33532f8faf955b656
Crosseye Jack <jack@crosseyejack.com>
2014-09-17 01:23:57 +0100

    Started to lay the ground work down.
    Had a lovely dinner, soak in the tub a cup of coffee and time to get some
    work done. Even though it is like 1:30 am. It needs to get done.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,97 @@
  * Created on 16 September 2014, 16:14
  */
 
+/*
+ * Plan of action
+ * OK so the plan is this:-
+ * - Create a deamon application that waits for an interrupt from the i2c bridge
+ * upon interrupt read the i2c data, process it and pass it onto openhab via it's
+ * REST API - rinse and repeat
+ * 
+ * - Create a FIFO and listen for comms, When OpenHAB (or any other app) needs to
+ * comminucate with the i2c bridge it will write to a FIFO which this app will read
+ * process and send to the i2c bridge.
+ * 
+ * for quickness I am going am probbally going tot be using wiringPi for interrupt,
+ * i2c, forking, etc
+ * 
+ * I could create a binding in OPENHAB to do this but I'm being lazy :-p
+ */
+
 #include <cstdlib>
 #include <wiringPiI2C.h>
+#include <string.h>
+#include <string>
+
+// main header file
+#include "main.h"
 
 using namespace std;
 
 /*
- * 
+ * Program entry point
  */
 int main(int argc, char** argv) {
+	
+	// Set up the signal interrupts
+	signal(SIGINT, SignalHandler); 
+	signal(SIGTERM, SignalHandler);
 
+	// Thread the "Listen to bridge" element
+	
 	return 0;
 }
 
+
+// Keyboard Interrupt Handler. To stop the main loop and allow the application to close gracefully.
+void SignalHandler(int Reason) {
+	//running = false;
+#ifdef DEBUG_PRINT
+	std::cout << "\r\nQuitting: " << Reason << std::endl;
+#endif
+	// TODO Shutdown the LED Flasher thread
+	digitalWrite(LED_RUNNING, LOW);
+	exit(EXIT_SUCCESS);
+}
+
+// Daemonise the application
+static void daemonise(void) {
+	pid_t pid, sid;
+
+	/* already a daemon */
+	if (getppid() == 1) return;
+
+	/* Fork off the parent process */
+	pid = fork();
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+	/* If we got a good PID, then we can exit the parent process. */
+	if (pid > 0) {
+		std::cout << "Forked into the background. (" << pid << ")" << std::endl;
+		exit(EXIT_SUCCESS);
+	}
+
+	/* At this point we are executing as the child process */
+
+	/* Change the file mode mask */
+	umask(0);
+
+	/* Create a new SID for the child process */
+	sid = setsid();
+	if (sid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	/* Change the current working directory.  This prevents the current
+	   directory from being locked; hence not being able to remove it. */
+	if ((chdir("/")) < 0) {
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Close out the standard file descriptors */
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+	std::cout << "Daemonized the app" << std::endl;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.h
+ * Author: srv
+ *
+ * Created on 17 September 2014, 01:14
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+void SignalHandler(int Reason); // Signal interupt handle
+static void daemonise(void);	// daemonise the application
+
+#endif	/* MAIN_H */
+
