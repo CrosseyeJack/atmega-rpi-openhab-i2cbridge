b86597b2234f5d9a591e3b41ecd2a63a73a860db
Crosseye Jack <jack@crosseyejack.com>
2014-09-17 15:20:17 +0100

    Created listener.h to start work on the i2c bridge listener
    I also need to re-read something :-p

--- listener.h
+++ listener.h
@@ -5,11 +5,15 @@
  * Created on 17 September 2014, 01:30
  */
 
+// This thread waits for an interrupt from the i2c bridge, reads it out,
+// processes it then hands of that the data to openHAB
+
 #ifndef LISTENER_H
 #define	LISTENER_H
 
-// #include "listener.cpp"
-
+void worker_thread_listener() {
+	
+}
 
 #endif	/* LISTENER_H */
 
